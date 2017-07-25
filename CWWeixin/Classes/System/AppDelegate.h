4b052bf8e32e6b79e899b2cccc821c00a45b2bc5
Coulson_Wang <wangyuanyi1993@126.com>
2017-07-25 23:41:45 +0800

    初始化项目，集成CocoaPods

---
+++ CWWeixin/Classes/System/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CWWeixin
+//
+//  Created by Coulson_Wang on 2017/7/25.
+//  Copyright © 2017年 Coulson_Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
